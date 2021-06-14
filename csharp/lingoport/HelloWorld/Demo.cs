4689a2c1bf5b04e597b90304010875df73a635e5
rthanLingoport <84477108+rthanLingoport@users.noreply.github.com>
2021-06-14 15:41:33 -0600

    Update Demo.cs

--- csharp/lingoport/HelloWorld/Demo.cs
+++ csharp/lingoport/HelloWorld/Demo.cs
@@ -16,6 +16,7 @@ namespace HelloWorld
         CharSet charset = CharSet.Auto;
         CaseInsensitiveComparer comparer = new CaseInsensitiveComparer();
         String lowercase = "U.S Capitals".ToLower();
+        String test = "This was added on 06-14 to test Jenkins.";
 
         public void doSomething()
         {
